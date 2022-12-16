7472b8c565c23cfd4d815fff643ddcca1c7a4404
ceda-dev-01 <l.lezzerini@coopceda.it>
2022-12-16 08:05:50 +0100

    First commit

---
+++ src/main/java/al/polis/jap2223concurrency/Jap2223Concurrency.java
@@ -0,0 +1,15 @@
+/*
+ */
+
+package al.polis.jap2223concurrency;
+
+/**
+ *
+ * @author use
+ */
+public class Jap2223Concurrency {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
