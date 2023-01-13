af59fea851c8d4f7998b2c09ea4adc4d04873dc7
ceda-dev-01 <l.lezzerini@coopceda.it>
2023-01-13 13:43:15 +0100

    Updated concurrency code

--- src/main/java/al/polis/jap2223concurrency/Jap2223Concurrency.java
+++ src/main/java/al/polis/jap2223concurrency/Jap2223Concurrency.java
@@ -1,7 +1,10 @@
 /*
  */
-
 package al.polis.jap2223concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  *
@@ -10,6 +13,52 @@ package al.polis.jap2223concurrency;
 public class Jap2223Concurrency {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Program started!");
+
+//        ExecutorService exes = Executors.newSingleThreadExecutor();
+//        ExecutorService exes = Executors.newFixedThreadPool(3);
+        ExecutorService exes = Executors.newCachedThreadPool();
+        Future f1 = exes.submit(() -> {
+            String t = Thread.currentThread().toString();
+            try {
+                System.out.println(t + "The very long query started now ...");
+                Thread.sleep(5_000);
+                System.out.println(t + "The very long query has finished!");
+            } catch (Exception e) {
+            }
+        });
+        exes.submit(() -> {
+            String t = Thread.currentThread().toString();
+            try {
+                System.out.println(t + "The second very long query started now ...");
+                Thread.sleep(6_000);
+                System.out.println(t + "The second very long query has finished!");
+            } catch (Exception e) {
+            }
+        });
+        exes.submit(() -> {
+            String t = Thread.currentThread().toString();
+            try {
+                System.out.println(t + "The 3rd very long query started now ...");
+                Thread.sleep(7_000);
+                System.out.println(t + "The 3rd very long query has finished!");
+            } catch (Exception e) {
+            }
+        });
+        exes.submit(() -> {
+            String t = Thread.currentThread().toString();
+            try {
+                System.out.println(t + "The 4th very long query started now ...");
+                Thread.sleep(8_000);
+                System.out.println(t + "The 4th very long query has finished!");
+            } catch (Exception e) {
+            }
+        });
+        System.out.println("Let me wait f1 is done ...");
+        try {
+            f1.get();
+        } catch (Exception e) {
+        }
+        System.out.println("Program Has finished! (but the query is yet running)");
     }
 }
